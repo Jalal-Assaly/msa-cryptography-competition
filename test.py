210ffc57e438002df7a33cdeefa4423495a218c5
Jalal Assaly <jalal.assaly@gmail.com>
2025-04-07 23:59:49 +0200

    Removed test.py file

--- test.py
+++
@@ -1,6 +0,0 @@
-def format_text(text, preserve_format=False):
-    if preserve_format:
-        return text
-    return ''.join(filter(str.isalpha, text)).upper()
-
-print(enumerate(format_text("Hello, world!", True)))
